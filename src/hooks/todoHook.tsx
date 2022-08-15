8fe0819b9f6aca18ed23f2f9576fb44c7ce08d1b
Pranav <pranav.fokmare.developer@gmail.com>
2022-08-15 21:52:12 +0530

    refactore todo

---
+++ src/hooks/todoHook.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { Todo } from '../Models';
+
+export interface TodoHookState{
+    todos: Todo[];
+    addToDo: (title: string) => void;
+    completeTodo: (todoId: number) => void;
+    deleteTodo: (todoId: number) => void;
+
+}
+export default function useToDoHook():TodoHookState {
+    const [todos, setTodos] = useState<Todo[]>([]);
+    function addToDo(title: string) {
+      let newTodos = [...todos];
+      newTodos.push({
+          id:Date.now(),
+        title: title,
+        isDone: false,
+      });
+      setTodos(newTodos);
+    }
+    function completeTodo(todoId:number){
+      setTodos(todos.map(t => 
+          (t.id === todoId)?{id:t.id, title:t.title, isDone:true}:
+          t
+          ));
+    }
+    function deleteTodo(todoId:number){
+        setTodos(todos.filter(t => t.id !== todoId));
+      }
+      return {todos,addToDo,completeTodo,deleteTodo};
+}
