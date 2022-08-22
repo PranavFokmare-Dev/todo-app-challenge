11be0b13e174c241a2b90dc2377515f92fea1abd
Pranav <pranav.fokmare.developer@gmail.com>
2022-08-22 22:21:25 +0530

    s2

---
+++ src/globalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing:border-box;
+  }
+  *, *::before,*::after{
+    box-sizing:border-box;
+  }
+`;
+ 
+export default GlobalStyle;
