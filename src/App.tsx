6450f8a037e1ab2ce4412f66ae2053c70ed1556f
Pranav <pranav.fokmare.developer@gmail.com>
2022-08-22 22:21:18 +0530

    s1

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TodoApp from './components/Todo';
-import TodoNavBar from './components/TodoNavBar';
-import styled from 'styled-components';
-import ThemeHandler from './components/ThemeHandler';
-var themeContext= React.createContext<string>("dark");
+import React, { useContext } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TodoApp from "./components/Todo";
+import TodoNavBar from "./components/TodoNavBar";
+import styled from "styled-components";
+import ThemeHandler, { IThemeStyles, themeContext, ThemeContextInterface } from "./components/ThemeHandler";
+import backgroundImg from "./images/bg-desktop-dark.jpg";
+import GlobalStyle from "./globalStyles";
 
-
+interface containerProps {
+  styles:IThemeStyles;
+}
 const Container = styled.div`
-  width:100%;
-  min-height:100vh;
-  background-color:hsl(235, 21%, 11%);
-  background-image: url("./static/media/bg-desktop-dark.73e47dbb723ebc772ef0.jpg");
-  background-repeat:no-repeat;
-`
-
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${(props: containerProps) =>props.styles.backgroundColor};
+  background-image: ${(props: containerProps) =>
+    "url(" + props.styles.backgroundImg + ")"};
+  background-repeat: no-repeat;
+`;
 
 const TodoContainer = styled.div`
-width:40%;
-margin:auto;
-padding-top:5%;
+  width:500px;
+  margin: auto;
+  padding-top: 5%;
+  padding-bottom:2em;
+  @media (max-width: 600px) {
+   width:90%; 
+  }
 `;
 
 function App() {
+  const t = useContext<ThemeContextInterface>(themeContext);
   return (
-    <Container>
-      <TodoContainer>
-        <ThemeHandler>
-          <TodoNavBar/>
-          <TodoApp/>
-        </ThemeHandler>
-      </TodoContainer>
-    </Container>
-
+    <div>
+      <GlobalStyle />
+      <Container styles={t.styles}>
+        <TodoContainer>
+            <TodoNavBar />
+            <TodoApp />
+        </TodoContainer>
+      </Container>
+    </div>
   );
 }
 
