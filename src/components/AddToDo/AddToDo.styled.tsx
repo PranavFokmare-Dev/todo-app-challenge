aa72c1c3801c0dbe9d8745a8703bb2b93ce30274
Pranav <pranav.fokmare.developer@gmail.com>
2022-08-22 22:22:12 +0530

    s8

---
+++ src/components/AddToDo/AddToDo.styled.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { IThemeStyles } from "../ThemeHandler";
+
+export const ToDoInput  = styled.input`
+ 
+  border:none;
+  width:100%;
+  background-color:inherit;
+  color:${(props:{styles:IThemeStyles})=>props.styles.textColor};
+
+  &:focus {
+        outline: none;
+
+    }
+`
+export const InputDiv = styled.div`
+   background-color:${(props:{styles:IThemeStyles})=>props.styles.todoListBgColor};
+  width:100%;
+  height:2.5em;
+  margin-bottom:2em;
+  display:flex;
+  align-items:center;
+  border-radius:5px;
+`
